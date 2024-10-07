1bb894e7c987c2d71a17186a9cd43330184cbcb3
hermanmansC3 <herman@convergenc3.com>
2024-10-07 12:19:16 +0200

    Currently Only On UAT

--- src/store/actions/recruitmentActions.js
+++ src/store/actions/recruitmentActions.js
@@ -71,9 +71,9 @@ export const submitDevForm = createAsyncThunk(
 
       const res = await axios.post(`${url}/recruitment/submitDevForm`, data,{headers});
       // Initial email sent out to applicant. Ensure that stage corresponds to any changes made on portal frontend stages
-      const emailApplicant = await axios.post(
+      await axios.post(
         `${url}/recruitment/sendDevEmail`, 
-        { email: data.email,name:data.name,stage:"CV to be screened" },{headers}
+        { email: data.email,name:data.name,stage:"CV to be screened"},{headers}
       );
 
       return res.data;
